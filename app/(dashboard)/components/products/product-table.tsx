fc3b9852afadfb6507b050ad29c93de1427b2ab8
tiorizkyramadhaniputra <tiorizky01@gmail.com>
2026-01-28 11:49:38 +0700

    Refactor TransactionModal to ProductTable component

--- app/(dashboard)/components/products/product-table.tsx
+++ app/(dashboard)/components/products/product-table.tsx
@@ -1,146 +1,77 @@
-import Button from "@/app/(landing)/components/ui/button";
-import Modal from "../ui/modal";
-import ImageUploadPreview from "../ui/image-upload-preview";
-import { useState } from "react";
-import Image from "next/image";
-import priceFormatter from "@/app/utils/price-formatter";
-import { FiCheck, FiX } from "react-icons/fi";
-import { Transaction } from "@/app/types";
 import { getImageUrl } from "@/app/lib/api";
+import { Product } from "@/app/types";
+import priceFormatter from "@/app/utils/price-formatter";
+import Image from "next/image";
+import { FiEdit2, FiTrash2 } from "react-icons/fi";
 
-type TTransactionModalProps = {
-  isOpen: boolean;
-  onClose: () => void;
-  transaction: Transaction | null;
-  onStatusChange: (id: string, status: "paid" | "rejected") => Promise<void>;
+type TProductTableProps = {
+  products: Product[];
+  onDelete?: (id: string) => void;
+  onEdit?: (product: Product) => void;
 };
 
-const TransactionModal = ({
-  isOpen,
-  onClose,
-  transaction,
-  onStatusChange,
-}: TTransactionModalProps) => {
-  const [isUpdating, setIsUpdating] = useState(false);
-
-  if (!transaction) return;
-
-  const handleStatusUpdate = async (status: "paid" | "rejected") => {
-    setIsUpdating(true);
-    try {
-      await onStatusChange(transaction._id, status);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsUpdating(false);
-    }
-  };
-
+const ProductTable = ({ products, onDelete, onEdit }: TProductTableProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} title="Verify Transactions">
-      <div className="flex gap-6">
-        <div className="min-w-50">
-          <h4 className="font-semibold text-sm mb-2">Payment Proof</h4>
-          {transaction.paymentProof ? (
-            <Image
-              src={getImageUrl(transaction.paymentProof)}
-              alt="payment proof"
-              width={200}
-              height={401}
-            />
-          ) : (
-            <div className="text-center p-4">
-              <p className="text-sm">No Payment proof uploaded</p>
-            </div>
-          )}
-        </div>
-        <div className="w-full">
-          <h4 className="font-semibold text-sm mb-2">Order Details</h4>
-          <div className="bg-gray-100 rounded-md flex flex-col gap-2.5 p-4  text-sm mb-5">
-            <div className="flex justify-between font-medium">
-              <div className="opacity-50">Date</div>
-              <div className="text-right">
-                {new Date(transaction.createdAt).toLocaleDateString("id-ID", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </div>
-            </div>
-            <div className="flex justify-between font-medium">
-              <div className="opacity-50">Customer</div>
-              <div className="text-right">{transaction.customerName}</div>
-            </div>
-            <div className="flex justify-between font-medium">
-              <div className="opacity-50">Contact</div>
-              <div className="text-right">{transaction.customerContact}</div>
-            </div>
-            <div className="flex justify-between gap-10 font-medium">
-              <div className="opacity-50 whitespace-nowrap">
-                Shipping Address
-              </div>
-              <div className="text-right">{transaction.customerAddress}</div>
-            </div>
-          </div>
-
-          <h4 className="font-semibold text-sm mb-2">Items Purchased</h4>
-
-          <div className="space-y-3">
-            {transaction.purchasedItems.map((item, index) => (
-              <div className="border border-gray-200 rounded-lg p-2 flex items-center gap-2">
-                <div className="bg-gray-100 rounded aspect-square w-8 h-8">
-                  <Image
-                    src={getImageUrl(item.productId.imageUrl)}
-                    width={30}
-                    height={30}
-                    alt="product image"
-                  />
+    <div className="bg-white rounded-xl border border-gray-200">
+      <table className="w-full text-left border-collapse">
+        <thead>
+          <tr className="border-b border-gray-200">
+            <th className="px-6 py-4 font-semibold">Product</th>
+            <th className="px-6 py-4 font-semibold">Category</th>
+            <th className="px-6 py-4 font-semibold">Price</th>
+            <th className="px-6 py-4 font-semibold">Stock</th>
+            <th className="px-6 py-4 font-semibold">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((data, index) => (
+            <tr
+              key={data._id}
+              className="border-b border-gray-200 last:border-b-0"
+            >
+              <td className="px-6 py-4 font-medium">
+                <div className="flex gap-2 items-center">
+                  <div className="aspect-square bg-gray-100 rounded-md">
+                    <Image
+                      src={getImageUrl(data.imageUrl)}
+                      width={52}
+                      height={52}
+                      alt={data.name}
+                      className="aspect-square object-contain"
+                    />
+                  </div>
+                  <span>{data.name}</span>
                 </div>
-                <div className="font-medium text-sm">{item.productId.name}</div>
-                <div className="font-medium ml-auto text-sm">
-                  {item.qty} units
+              </td>
+              <td className="px-6 py-4 font-medium">
+                <div className="rounded-md bg-gray-200 px-2 py-1 w-fit">
+                  {data.category.name}
                 </div>
-              </div>
-            ))}
-          </div>
-          <div className="flex justify-between  text-sm mt-6">
-            <h4 className="font-semibold">Total </h4>
-            <div className="text-primary font-semibold">
-              {priceFormatter(parseInt(transaction.totalPayment))}
-            </div>
-          </div>
-          <div className=" flex justify-end gap-5 mt-12">
-            {isUpdating ? (
-              <div className="text-center">Updating...</div>
-            ) : (
-              <>
-                <Button
-                  className="text-primary! bg-primary-light! rounded-md"
-                  size="small"
-                  onClick={() => handleStatusUpdate("rejected")}
-                  disabled={isUpdating}
+              </td>
+              <td className="px-6 py-4 font-medium">
+                {priceFormatter(data.price)}
+              </td>
+              <td className="px-6 py-4 font-medium">{data.stock} units</td>
+              <td className="px-6 py-7.5 flex items-center gap-3 text-gray-600">
+                <button
+                  onClick={() => onEdit?.(data)}
+                  className="cursor-pointer"
                 >
-                  <FiX size={20} />
-                  Reject
-                </Button>
-                <Button
-                  className="text-white! bg-[#50C252]! rounded-md"
-                  size="small"
-                  onClick={() => handleStatusUpdate("paid")}
-                  disabled={isUpdating}
+                  <FiEdit2 size={20} />
+                </button>
+                <button
+                  onClick={() => onDelete?.(data._id)}
+                  className="cursor-pointer"
                 >
-                  <FiCheck size={20} />
-                  Approve
-                </Button>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </Modal>
+                  <FiTrash2 size={20} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-export default TransactionModal;
+export default ProductTable;
